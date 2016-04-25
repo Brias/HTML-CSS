1a0a79b95ac12c278be7b55e6235bf24b3a577e6
Matthias Bräuer <matthias.braeuer@arcor.de>
2016-04-25 15:55:18 +0200

    bla

--- js/app/addSlider.js
+++ js/app/addSlider.js
@@ -1,10 +1,17 @@
 /**
  * Created by Matze on 19.04.2016.
  */
-$('.slider').load('slider.html', function(){
-  $('.slider').slider({
-    full_width: true,
-    indicators: false,
-    interval: 3000
+function addSlider(slides) {
+  $.get('slider.html', function (data) {
+    var $slider = $('.slider'),
+        template = _.template(data);
+
+    $slider.html(template({slides: slides}));
+
+    $slider.slider({
+      full_width: true,
+      indicators: false,
+      interval: 3000
+    });
   });
-});
+}

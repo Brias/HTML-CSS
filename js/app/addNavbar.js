4c4e109306a739c55f9955f5a1b9af3b719982bd
Matthias Bräuer <matthias.braeuer@arcor.de>
2016-04-30 02:15:07 +0200

    xdghfgh

--- js/app/addNavbar.js
+++ js/app/addNavbar.js
@@ -1,17 +1,8 @@
 /**
  * Created by Matze on 11.04.2016.
  */
-var loc = window.location.pathname,
-    dir = loc.substring(0, loc.lastIndexOf('/'));
-
-if(dir.substr(dir.lastIndexOf('/')) === "/html"){
-  $('#navbar').load('navbar.html', checkForActive);
-}else{
-  $('#navbar').load('html/navbar.html', checkForActive);
-}
-
-function checkForActive(){
-  switch(loc.substring(loc.lastIndexOf('/') + 1, loc.length)){
+function checkForActive() {
+  switch (loc.substring(loc.lastIndexOf('/') + 1, loc.length)) {
     case 'development.html':
       setActive('development');
       break;
@@ -30,6 +21,40 @@ function checkForActive(){
   }
 }
 
-function setActive(id){
+function setActive(id) {
   $('#' + id).addClass('active');
+}
+
+function toggleNavbar() {
+  var $nav_list = $('.responsive-nav-list');
+
+  if($nav_list.is(':hidden')){
+    $nav_list.show('slow')
+  } else {
+    $nav_list.fadeOut('slow');
+  }
+}
+
+function onResize() {
+  console.log("sdd");
+  if($(window).width() > 625) {
+    $('.responsive-nav-list').css("display", "block");
+  } else {
+    $('.responsive-nav-list').css("display", "none");
+  }
+}
+
+function onNavbarLoaded() {
+  checkForActive();
+  $(window).resize(onResize);
+  $('.responsive-nav-toggle').click(toggleNavbar);
+}
+
+var loc = window.location.pathname,
+    dir = loc.substring(0, loc.lastIndexOf('/'));
+
+if (dir.substr(dir.lastIndexOf('/')) === "/html") {
+  $('#navbar').load('navbar.html', onNavbarLoaded);
+} else {
+  $('#navbar').load('html/navbar.html', onNavbarLoaded);
 }
